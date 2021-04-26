9e104684c21ca7c7879c7e6040d0059f986501dc
akif rahman <akif.eee.200105175@aust.edu>
2021-04-26 11:46:51 +0600

    Alter way to write

--- 100 days of  code/Day-4 Randomisation & Lists/Banker rolette.py
+++ 100 days of  code/Day-4 Randomisation & Lists/Banker rolette.py
@@ -3,5 +3,6 @@ name_string = input("Give me erverybody's name\n")
 
 name = name_string.split(",")
  
-person_who_will_pay = random.choice(name) 
+random_nmae = random.randint(0, 2)
+person_who_will_pay = name[random_nmae] 
 print(f"{person_who_will_pay} is going to buy the mill today!")
