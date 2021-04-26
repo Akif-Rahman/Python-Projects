57a17983fd5a6762c240d375b41ad8f57473f3d9
akif rahman <akif.eee.200105175@aust.edu>
2021-04-26 16:27:25 +0600

    Treasure map

---
+++ 100 days of  code/Day-4 Randomisation & Lists/Treasure Map.py
@@ -0,0 +1,14 @@
+
+row1 = ['⬜️', '⬜️', '⬜️']
+row2 = ['⬜️', '⬜️', '⬜️']
+row3 = ['⬜️', '⬜️', '⬜️']
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+x = input("Where do you want to put the treasure? ")
+
+coloumn = int(x[0])
+row = int(x[1])
+
+selected_row = map[row-1]
+selected_row[coloumn-1] = "X"
+print(f"{row1}\n{row2}\n{row3}")
