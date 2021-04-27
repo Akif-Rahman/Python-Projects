3207f5206af1df0db5e2f0795fcb13516d26879c
akif rahman <akif.eee.200105175@aust.edu>
2021-04-27 17:29:27 +0600

    dict

---
+++ 10 real world applications/The basic data types/dictonaries.py
@@ -0,0 +1,7 @@
+tuesday_temperature = [9.1, 8.5, 7.5] #items in a list made of values.
+students_grades = {"Marry":9.1, "Sim":8.8, "John":7.5} #item in a dictonary made of keys and values.
+
+mysum = sum(students_grades.values())
+length = len(students_grades)
+mean = mysum / length
+print(mean)
