a22c1a4c1ac6fef45cb64efe0656145c56e8c51f
akif rahman <akif.eee.200105175@aust.edu>
2021-05-09 15:48:13 +0600

    3rd

--- Python Apps/App-1/Antipod.py
+++ Python Apps/App-1/Antipod.py
@@ -1,11 +1,14 @@
 latitude = float("40.09")
-longitude = float("3.47")
+longitude = float("220")
 
 antipode_latitude = latitude.__mul__(int("-1"))
 
 if longitude.__le__(float("0")):
     antipode_longitude = longitude.__add__(float("180")) 
-    
+elif longitude.__eq__(float("0")):
+    antipode_longitude = float("180")    
+elif longitude.__gt__(float("180")):
+    antipode_longitude = str("Invalid longitude")    
 else:
     antipode_longitude = longitude.__sub__(float("180"))
 
